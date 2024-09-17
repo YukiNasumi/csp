c970088bfa59d74d6765f68499a8b9ebd713595a
YukiNasumi <ireneenriquez657@gmail.com>
2024-09-17 11:56:42 +0800

    pass csp33/3 all by myself

--- csp33/3/matrix.cpp
+++ csp33/3/matrix.cpp
@@ -23,15 +23,6 @@ bool is_col_empty(int x,int y,int r){
     }
     return true;
 }
-void swap_col(int x,int y1,int y2,int r){
-    for(int i=0;i<r;i++){
-        double mid;
-        mid = matrix[x+i][y1];
-        matrix[x+i][y1] = matrix[x+i][y2];
-        matrix[x+i][y2] = mid;
-    }
-    return ;
-}
 void swap_row(int x1,int x2,int y,int c){
     for(int i=0;i<c;i++){
         double mid;
@@ -49,20 +40,14 @@ void modify_row(int x,int y,int target_r,int c){
         matrix[target_r][y+i]-=factor*matrix[x][y+i];
     }
 }
-bool is_row_empty(int x,int y,int c){
-    for(int i=0;i<c;i++){
-        if(fabs(matrix[x][y+i]>=1e-5)) return false;
-    }
-    return true;
-}
 int rank_transform(int x,int y, int r,int c){
     if(x>=row||y>=column) return 1;
     if(is_col_empty(x,y,r)) {
-        rank_transform(x+1,y+1,r-1,c-1);
+        rank_transform(x,y+1,r,c-1);
         return 1;};
     if(fabs(matrix[x][y])<=1e-5){//the start point element is zero
         int i=1;
-        for(;i<r&&is_row_empty(x+i,y,c);i++);
+        for(;i<r&&(matrix[x+i][y]<=1e-5);i++);
         swap_row(x,x+i,y,c);
 
     }
@@ -139,46 +124,9 @@ signed main(){
         }
         row = r;
         column = c;
-        //show_matrix(r,c);
-        int  j, k, len, s, t, z;
-        double tmp = 0;
-        double p;
-        t = row,m = column;
-        for(int i =0;i<row;i++){
-            for(int j=0;j<column;j++){
-                a[i+1][j+1]=matrix[i][j];
-            }
-        }
-        for (i = 1; i <= t; i++) {    
-            // 检验全为0则忽略该行    
-            for (j = 1; j <= m; j++)
-                if (a[i][j] != 0)
-                    break;
-            if (j > m) continue;
-            if (a[i][i] == 0) { // 如果第i列为0，向下找一个不为0的整行交换 
-                for (j = i + 1; j <= t && a[j][i] == 0; j++);
-                for (k = 1; k <= m; k++)
-                    tmp = a[i][k],
-                    a[i][k] = a[j][k],
-                    a[j][k] = tmp;
-            }
-            for (j = i + 1; j <= t; j++) { // 将后面每一行减去第i行相应倍数，使第i列变成0 
-                p = a[j][i] / a[i][i];
-                for (k = i; k <= m; k++)
-                    a[j][k] = a[j][k] - a[i][k] * p;
-            }
-        }
-        z = 0;
-        for (i = 1; i <= t; i++) { // 统计不全为0的行数，即矩阵的秩 
-            for (j = 1; j <= m && a[i][j] == 0; j++);
-            if (j <= m) z++;
-        }
-        if (z < m) printf("Y\n");
-        else printf("N\n");
-    
-        /*rank_transform(0,0,r,c);
+        rank_transform(0,0,r,c);
         if(cal_rank()<c) cout << 'Y' << endl;
-        else cout << 'N' << endl;*/
+        else cout << 'N' << endl;
         memset(matrix,0,sizeof(matrix));
     }
     return 0;
