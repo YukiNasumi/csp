a127bd7b069977b1ca5ed059e9e4e1dec028b8e9
YukiNasumi <ireneenriquez657@gmail.com>
2024-09-21 13:57:28 +0800

    finish 07/3

--- 07/cpp/3.cpp
+++ 07/cpp/3.cpp
@@ -1,23 +1,87 @@
 #include<bits/stdc++.h>
 using namespace std;
-int p;
+int P;
 string current_dir;
 vector<string>split(string& dir){
     int start=0,end = 0;
     vector<string>res;
-    if(dir[0]=='/') {
-        res.push_back("/");
-        dir.replace(0,1,"");
+    int l = dir.length();
+    if(dir[0]=='/') res.push_back("/");
+    for(int i =0;i<l;i++){
+        char c = dir[i];
+        if(c=='/'){
+            end = i;
+            if(end-start)
+                res.push_back(dir.substr(start,end-start));
+            start = end+1;
         }
-    start = dir.find("/",end);
-    end = dir.find("/",start+1);
-    while(end<dir.length()&&end!=string::npos){
-        res.push_back(dir.substr(start,end-start))
     }
+    if(dir[l-1]!='/'){
+        res.push_back(dir.substr(start,l));
+    }
+    return res;
 
 }
+typedef struct node{
+    string name;
+    map<string,node*>children;
+}node;
+node* root;
 int main(){
-    cin >> p;
+    cin >> P;
     cin >> current_dir;
-
+    vector<string>l = split(current_dir);
+    /*for(auto &a:l) cout << a << endl;
+    return 0;*/
+    for(int k=0;k<P;k++){
+        root = new node;
+        node* p = root;
+        node* parent = root;
+        for(vector<string>::iterator it=l.begin();it!=l.end();it++){
+            string name = *it;
+            p->name = name;
+            p->children = {
+                {".",p},
+                {"..",parent},
+                {"/",root}
+            };
+            if((it+1)==l.end()){
+                break;
+            }
+            node * child = new node;
+            p->children.insert(make_pair(*(it+1),child));
+            parent =p;
+            p=child;
+        }
+            //now p points to the current dir
+        string path;
+        cin >> path;
+        vector<string>path2 = split(path);
+        node* cwd=p;//current work dir
+        for(vector<string>::iterator it=path2.begin();it!=path2.end();it++){
+            string name = *it;
+            if(!cwd->children.count(name)){
+                node* newchild = new node;
+                newchild->name = name;
+                newchild->children.insert(make_pair("/",root));
+                newchild->children.insert(make_pair(string("."),newchild));
+                newchild->children.insert(make_pair(string(".."),cwd));
+                cwd->children.insert(make_pair(name,newchild));
+            }
+            cwd = cwd->children[name];
+        }
+        list<string>ans;
+        do{
+            ans.push_front(cwd->name);
+            cwd = cwd->children[string("..")];
+        }while(cwd);
+        for(list<string>::iterator it=ans.begin();it!=ans.end();it++){
+            string name = *it;
+            cout << name;
+            if((++it)!=ans.end()&&name!="/") cout<<"/";
+            it--;
+        }
+        cout << endl;
+    }
+    return 0;
 }
